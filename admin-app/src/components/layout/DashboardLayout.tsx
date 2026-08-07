8782309b4d734632d5632138d38d5ef961044a08
Manoj <seenumanoj2006@gmail.com>
2026-08-07 12:04:04 +0530

    feat: make entire application responsive on mobile with off-canvas drawers

--- admin-app/src/components/layout/DashboardLayout.tsx
+++ admin-app/src/components/layout/DashboardLayout.tsx
@@ -15,11 +15,10 @@ export default function DashboardLayout() {
     <div className="min-h-screen flex">
       <Sidebar />
       <div
-        className="flex-1 flex flex-col transition-all duration-300"
-        style={{ marginLeft: sidebarCollapsed ? 72 : 260 }}
+        className={`flex-1 flex flex-col transition-all duration-300 w-full ${sidebarCollapsed ? 'md:ml-[72px]' : 'md:ml-[260px]'}`}
       >
         <Header />
-        <main className="flex-1 p-6 overflow-auto">
+        <main className="flex-1 p-4 md:p-6 overflow-auto">
           <Outlet />
         </main>
       </div>
